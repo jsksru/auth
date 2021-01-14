6692472b2157d50b0b84c4d6c54f800333349280
jsksru <mail@jsks.ru>
2021-01-14 10:15:42 +0500

    change login logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
+import config from '../config';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+
+const ACCESS_SECRET = config.tokens.access.secret;
+const ACCESS_EXPIRE = config.tokens.access.expire;
+const REFRESH_SECRET = config.tokens.refresh.secret;
+const REFRESH_EXPIRE = config.tokens.refresh.expire;
 
 interface IUser {
   id: number;
@@ -35,8 +41,20 @@ export const login = async (req: Request, res: Response) => {
     const verifyPassword = await bcrypt.compare(password, user.password);
     if (!verifyPassword) throw new Error('Bad username or password');
 
-    const token = jwt.sign(user, 'secret');
-    res.status(200).json({ succeed: true, token });
+    const accessToken = jwt.sign(user, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE });
+    const refreshToken = jwt.sign(user, REFRESH_SECRET, { expiresIn: REFRESH_EXPIRE });
+
+    console.log(req.headers['user-agent']);
+    console.log(req.socket.remoteAddress);
+
+    res.cookie('refresh', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      domain: 'localhost',
+    });
+    res.set('Authorization', 'Bearer ' + accessToken);
+    res.status(200).json({ succeed: true });
   } catch (err) {
     res.status(401).json({ error: true, message: err.message });
   }
