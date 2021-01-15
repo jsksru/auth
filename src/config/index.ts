c197c24617cc31ddc9e10d5ee8e237ad7b43e589
jsksru <mail@jsks.ru>
2021-01-15 17:51:04 +0500

    removed cookie settings for sessions

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,18 +15,6 @@ const config = {
       expire: '6m'
     }
   },
-  cookiesConfig: {
-    name: 'sessionID',
-    secret: 'EEHMv+DY13+W+V6TbOf8ZnaXkKiDSivwqgCj/a8UPz/vl9UceGm53fWRHOfeyDiolT9LiUoXu6VqJT8oTh4pgQ==',
-    cookie: {
-      secure: true,
-      httpOnly: true,
-      sameSite: true,
-      path: '/',
-      domain: 'localhost',
-      expires: new Date(Date.now() + 60 * 60 * 1000)
-    }
-  },
   https: {
     key: './https_private_key.key',
     cert: './https_certificate.crt'
