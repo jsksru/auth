0f8136742a66763d4a0edb11f1b4db4ad1583e26
jsksru <mail@jsks.ru>
2021-01-18 19:40:31 +0500

    change config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,21 @@
 const config = {
   api: {
+    domain: 'localhost',
     port: 3043,
     prefix: '/api/v1',
   },
   tokens: {
     access: {
-      type: 'access',
       secret: 'u2AMvFoxGP9rnbWcTBeK2VEMXUg4y9urj6eNvuWfZOuI7o0cIdDbDpBjvmXNSo39VNCCsPOpYvsQxNiAqK8Bzw==',
-      expire: '5m'
+      expire: '5m',
     },
     refresh: {
-      type: 'refresh',
-      secret: '11jFyIA90wp4YGzzWbsLiPhSLGwlsScnRD+joYeKe6u/mx0+lOrtf8OFsQFl3MSgBiHTDO1+/HlhQuRRRX/DzA==',
-      expire: '6m'
+      lifetime: '14d',
     }
   },
   https: {
     key: './https_private_key.key',
-    cert: './https_certificate.crt'
+    cert: './https_certificate.crt',
   }
 };
 
