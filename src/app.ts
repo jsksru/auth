96a2ec0cfaa8e035701d46af8f9b3309ab4d1249
jsksru <mail@jsks.ru>
2021-01-18 19:39:54 +0500

    cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import config from './config';
 import express from 'express';
 import Router from './routes';
+import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/errors.middleware';
 
 const app = express();
 const API_PREFIX = config.api.prefix;
 
+app.disable('x-powered-by');
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(API_PREFIX, Router);
 
