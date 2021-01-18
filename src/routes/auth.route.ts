8cfc59874789fbdf59486b7f89a17d63d13c86cc
jsksru <mail@jsks.ru>
2021-01-18 19:41:21 +0500

    added refresh route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,5 +4,6 @@ import authController from '../controllers/auth.controller';
 const Router = express.Router();
 
 Router.post('/login', authController.login);
+Router.post('/refresh', authController.refresh);
 
 export default Router;
