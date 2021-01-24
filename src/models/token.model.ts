762264ff3ecb6b46cefda8c323eb7a0bfb89ae86
jsksru <mail@jsks.ru>
2021-01-24 14:09:54 +0500

    token model

---
+++ src/models/token.model.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { v4 as uuid } from 'uuid';
+
+const { Schema, model, Types } = mongoose;
+
+export default model('Token', new Schema({
+  userId: {
+    type:  Types.ObjectId,
+    required: true,
+    index: true,
+  },
+  refresh: {
+    type: String,
+    required: true,
+    default: uuid(),
+    index: true,
+  },
+  userAgent: String,
+  ipAddress: String,
+  lastUpdate: {
+    type: Date,
+    default: Date.now(),
+  },
+  blocked: {
+    type: Boolean,
+    default: false
+  }
+}));
