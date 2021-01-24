d422b10f07a91489f41eee2044f9b0d53a7e4f3f
jsksru <mail@jsks.ru>
2021-01-24 14:10:30 +0500

    connect to db

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,40 +3,13 @@ import config from '../config';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { v4 as uuid } from 'uuid';
+import UserModel from '../models/user.model';
+import TokenModel from '../models/token.model';
 
 const ACCESS_SECRET = config.tokens.access.secret;
 const ACCESS_EXPIRE = config.tokens.access.expire;
 const API_PREFIX = config.api.prefix;
 const API_DOMAIN = config.api.domain;
-
-interface IUser {
-  id: number;
-  username: string;
-  password: string;
-}
-interface IToken {
-  id: string;
-  userId: number;
-  refresh: string;
-  userAgent: string | null;
-  ipAddress: string | null;
-  lastUpdate: number;
-  blocked: boolean;
-}
-
-const users: IUser[] = [
-  {
-    id: 1,
-    username: 'user',
-    password: '$2b$10$AeRxOfzlyD0k7q4I6lGpiO8SQjnDDt1NEZiOFrvE0Of.rJfTaKROa'
-  },
-  {
-    id: 2,
-    username: 'admin',
-    password: '$2b$10$/KXDNWVypiEZaheDQt5cae./soY0zru8DB/GyVKoN.lDmBubwHPZW'
-  }
-];
-const tokens: IToken[] = [];
 
 export const login = async (req: Request, res: Response) => {
   try {
@@ -46,27 +19,25 @@ export const login = async (req: Request, res: Response) => {
     if (!username) throw new Error('Empty username');
     if (!password) throw new Error('Empty password');
 
-    const user = users.find(u => u.username === username.trim().toLocaleLowerCase());
+    const user = await UserModel.findOne({ username: username.toLocaleLowerCase().trim() });
     if (!user) throw new Error('User not found');
 
     const verifyPassword = await bcrypt.compare(password, user.password);
     if (!verifyPassword) throw new Error('Bad username or password');
 
-    const accessToken = jwt.sign(user, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE });
+    const accessToken = jwt.sign({ userId: user._id, username: user.username }, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE });
 
     const userAgent = req.headers['user-agent'] || null;
     const ipAddress = req.socket.remoteAddress || null;
     const newUuid = uuid();
 
-    tokens.push({
-      id: uuid(),
-      userId: user.id,
+    const token = new TokenModel({
+      userId: user._id,
       refresh: newUuid,
       userAgent,
-      ipAddress,
-      lastUpdate: Date.now(),
-      blocked: false,
+      ipAddress
     });
+    await token.save();
 
     res.cookie('refresh', newUuid, {
       httpOnly: true,
@@ -85,23 +56,25 @@ export const login = async (req: Request, res: Response) => {
 
 export const refresh = async (req: Request, res: Response) => {
   try {
-    const token = req.cookies.refresh as string || null;
+    const requestToken = req.cookies.refresh as string || null;
     const userAgent = req.headers['user-agent'] || null;
     const ipAddress = req.socket.remoteAddress || null;
     const newUuid = uuid();
 
-    const tokenIndex = tokens.findIndex(t => t.refresh === token);
-    if (tokenIndex === -1) throw new Error('Bad refresh token');
-    if (tokens[tokenIndex].blocked) throw new Error('Token blocked');
-    if (tokens[tokenIndex].ipAddress !== ipAddress || tokens[tokenIndex].userAgent !== userAgent) throw new Error('Ip or User-Agent changed');
+    const resultToken = await TokenModel.findOne({ refresh: requestToken });
+    if (!resultToken) throw new Error('Bad refresh token');
+    if (resultToken.blocked) throw new Error('Token blocked');
+    if (resultToken.ipAddress !== ipAddress || resultToken.userAgent !== userAgent) throw new Error('Ip or User-Agent changed');
 
-    const user = users.find(u => u.id === tokens[tokenIndex].userId);
+    const user = UserModel.findOne({ _id: resultToken.userId });
     if (!user) throw new Error('User not found');
 
-    tokens[tokenIndex].refresh = newUuid;
-    tokens[tokenIndex].lastUpdate = Date.now();
+    TokenModel.findByIdAndUpdate({ _id: resultToken._id }, {
+      refresh: newUuid,
+      lastUpdate: Date.now()
+    });
 
-    const accessToken = jwt.sign(user, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE });
+    const accessToken = jwt.sign({ userId: user._id, username: user.username }, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRE });
 
     res.cookie('refresh', newUuid, {
       httpOnly: true,
