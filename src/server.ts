cc691ec1689f627c4f8e07957e0e946243b5b582
jsksru <mail@jsks.ru>
2021-01-24 14:09:17 +0500

    added mongo

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,18 @@ import config from './config';
 import https from 'https';
 import fs from 'fs';
 import app from './app';
+import mongoose from 'mongoose';
 
 const PRIVATE_KEY = fs.readFileSync(config.https.key, 'utf8');
 const CERTIFICATE = fs.readFileSync(config.https.cert, 'utf8');
 const API_PORT = config.api.port;
+const DB_STRING = config.db.connectionString;
 
-const httpsServer = https.createServer({ key: PRIVATE_KEY, cert: CERTIFICATE }, app);
-
-httpsServer.listen(API_PORT, '0.0.0.0', () => console.log(`Server listening on ${API_PORT} port...`));
+mongoose.connect(DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 })
+  .then(() => {
+    https.createServer({ key: PRIVATE_KEY, cert: CERTIFICATE }, app)
+      .listen(API_PORT, '0.0.0.0', () => console.log(`Server listening on ${API_PORT} port...`));
+  })
+  .catch(err => {
+    console.error('DB connection error:', err);
+  });
