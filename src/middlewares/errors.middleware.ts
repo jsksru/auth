ccf7034f40afd4ff70f4916f1029bd07963efb3b
jsksru <mail@jsks.ru>
2021-01-26 20:36:02 +0500

    return

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import AnswerCreator from '../utils/answerCreator';
 
 export const notFound = (req: Request, res: Response) => {
-  AnswerCreator.error.notFound(res, 'Not Found!');
+  return AnswerCreator.error.notFound(res, 'Not Found!');
 };
 
 export const allErrors = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  AnswerCreator.error.serverError(res, err.message);
+  return AnswerCreator.error.serverError(res, err.message);
 };
 
 export default {
