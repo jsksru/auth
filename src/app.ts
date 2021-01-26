b9d52d0f7379d9d11da3f8d1276df46cbd738558
jsksru <mail@jsks.ru>
2021-01-26 20:35:04 +0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import config from './config';
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import Router from './routes';
 import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/errors.middleware';
 
 const app = express();
 const API_PREFIX = config.api.prefix;
+const CORS_ENABLE = config.api.cors.enable;
+const CORS_WHITELIST = config.api.cors.whitelist;
 
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
+  origin: CORS_ENABLE ? CORS_WHITELIST : '*',
   exposedHeaders: 'Authorization',
+  credentials: true,
 }));
+app.use(morgan('tiny'));
 
 app.use(API_PREFIX, Router);
 
