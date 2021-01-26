74420fb5acafeb9967e28a3612dadc59e09cc9f5
jsksru <mail@jsks.ru>
2021-01-26 20:35:45 +0500

    more config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,30 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const stb = (str: string): boolean => str === 'true';
+
 const { env } = process;
 
 const config = {
   api: {
     domain: env.API_DOMAIN as string || 'localhost',
-    port: Number(env.API_PORT) || 3043,
     prefix: env.API_PREFIX as string || '/api/v1',
+    mode: {
+      http: {
+        enable: stb(env.HTTP_ENABLE as string || 'true'),
+        port: Number(env.HTTP_PORT as string) || 3030,
+      },
+      https: {
+        enable: stb(env.HTTPS_ENABLE as string || 'false'),
+        port: Number(env.HTTPS_PORT as string) || 3031,
+        key: env.HTTPS_KEY_FILE as string || './https_private_key.key',
+        cert: env.HTTPS_CERT_FILE as string || './https_certificate.crt',
+      },
+    },
+    cors: {
+      enable: stb(env.CORS_ENABLE as string || 'false'),
+      whitelist: String(env.CORS_WHITELIST as string).split(','),
+    },
   },
   tokens: {
     access: {
@@ -15,17 +32,12 @@ const config = {
       expire: env.JWT_EXPIRE as string || '10m',
     },
     refresh: {
-      lifetime: env.REFRESH_LIFETIME as string || '14d',
-    }
-  },
-  https: {
-    enable: Boolean(env.HTTPS_ENABLE as string || 'true'),
-    key: env.HTTPS_KEY_FILE as string || './https_private_key.key',
-    cert: env.HTTPS_CERT_FILE as string || './https_certificate.crt',
+      lifetime: Number(env.REFRESH_LIFETIME as string) || 86400,
+    },
   },
   db: {
-    connectionString: env.MONGO_DB_STRING as string || '',
-  }
+    connectionString: env.MONGO_CONNECTION_STRING as string || '',
+  },
 };
 
 export default config;
