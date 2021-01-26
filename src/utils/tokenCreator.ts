e4b087ed8ec52df181e70dc0903ffd0403598bab
jsksru <mail@jsks.ru>
2021-01-26 20:36:35 +0500

    cookie options

--- src/utils/tokenCreator.ts
+++ src/utils/tokenCreator.ts
@@ -1,20 +1,24 @@
 import config from '../config';
 import { Response } from 'express';
+import AnswerCreator from './answerCreator';
 
 const API_PREFIX = config.api.prefix;
 const API_DOMAIN = config.api.domain;
+const HTTPS_ENABLE = config.api.mode.https.enable;
+const COOKIE_MAX_AGE = config.tokens.refresh.lifetime;
 
 export const sendTokens = (res: Response, newUuid: string, accessToken: string) => {
   res.cookie('refresh', newUuid, {
     httpOnly: true,
-    secure: true,
+    secure: HTTPS_ENABLE ? true : false,
     sameSite: 'strict',
     domain: API_DOMAIN,
-    path: API_PREFIX + '/auth/refresh'
+    path: API_PREFIX + '/auth/refresh',
+    expires: new Date(Date.now() + COOKIE_MAX_AGE),
   });
 
   res.set('Authorization', 'Bearer ' + accessToken);
-  return res.status(200).json({ succeed: true });
+  return AnswerCreator.success.emptyOk(res);
 };
 
 export default {
